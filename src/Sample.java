24de0b717716fb26a4fa3ca147516351e3fd63a5
RANJEET0501 <ranjeetmahato302@gmail.com>
2022-12-15 00:39:07 +0530

    first

---
+++ src/Sample.java
@@ -0,0 +1,24 @@
+
+public class Sample {
+
+	
+	public static void main(String[] args) {
+		
+		int n = 23457;
+		int count =0;
+		
+		while(n>0) {
+			n=n/10;
+			count=count++;
+		}
+			System.out.println(count);
+		
+			
+			
+		}
+		
+		
+		
+		
+	}
+
